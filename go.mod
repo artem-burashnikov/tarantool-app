aad479e3154a874a0bd01d29ef51b83459508870
artem.burashnikov <artem.burashnikov@gmail.com>
2025-03-24 23:08:44 +0300

    feat: add basic api entrypoint

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module tarantool-app
 
 go 1.24.1
+
+require github.com/tarantool/go-tarantool/v2 v2.3.0
+
+require (
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/shopspring/decimal v1.3.1 // indirect
+	github.com/tarantool/go-iproto v1.1.0 // indirect
+	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,6 +1,40 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/tarantool/go-tarantool/v2"
+	_ "github.com/tarantool/go-tarantool/v2/datetime"
+	_ "github.com/tarantool/go-tarantool/v2/decimal"
+	_ "github.com/tarantool/go-tarantool/v2/uuid"
+)
 
 func main() {
-	for {}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+
+	defer cancel()
+
+	dialer := tarantool.NetDialer{
+		Address:  "tarantool-storage:3301",
+		User:     "storage",
+		Password: "sesame",
+	}
+	opts := tarantool.Opts{
+		Timeout: time.Second,
+	}
+
+	conn, err := tarantool.Connect(ctx, dialer, opts)
+	if err != nil {
+		fmt.Println("Connection refused:", err)
+		return
+	}
+
+	fmt.Println("Connection established")
+
+	time.Sleep(5 * time.Second)
+
+	conn.CloseGraceful()
+	fmt.Println("Connection is closed")
 }
