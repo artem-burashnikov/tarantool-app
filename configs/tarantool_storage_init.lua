48bd5563a6f99541d5517aa421e6e2d008d9d320
artem.burashnikov <artem.burashnikov@gmail.com>
2025-03-27 17:57:03 +0300

    fix: tarantool space schema definition

--- configs/tarantool_storage_init.lua
+++ configs/tarantool_storage_init.lua
@@ -9,16 +9,15 @@ box.once("bootstrap", function()
     --- Create a space.
     box.schema.space.create('kv_storage')
 
+
     --- Define schema.
-    box.kv_storage:format({
+    box.space.kv_storage:format({
         { name = 'key', type = 'str' },
         { name = 'value', type = 'map' },
     })
 
     --- Add primary index.
-    box.space.kv_storage:create_index('primary', {
-        parts = { 'key' },
-    })
+    box.space.kv_storage:create_index('primary', { parts = { 'key' } })
 
     --- MsgPack serialization option.
     msgpack.cfg{
