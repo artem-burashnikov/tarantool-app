26cc8c3de934cebf3bc83631067b459ddb390d86
artem-burashnikov <artem.burashnikov@gmail.com>
2025-04-02 11:42:39 +0300

    refactor: clean up zap logger; remove middleware

---
+++ internal/interfaces/logger.go
@@ -0,0 +1,10 @@
+package interfaces
+
+type Logger interface {
+	Info(msg string, keysAndValues ...any)
+	Debug(msg string, keysAndValues ...any)
+	Warn(msg string, keysAndValues ...any)
+	Error(msg string, keysAndValues ...any)
+	Fatal(msg string, keysAndValues ...any)
+	Sync() error
+}
