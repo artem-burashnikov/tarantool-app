4d3c6f38c5bf555261b49399717008d96c686987
artem-burashnikov <artem.burashnikov@gmail.com>
2025-04-02 11:44:09 +0300

    refactor: update usecase interface and usage

---
+++ internal/usecases/usecase.go
@@ -0,0 +1,34 @@
+package usecases
+
+import (
+	"tarantool-app/internal/domain"
+	"tarantool-app/internal/interfaces"
+)
+
+type UserUseCase struct {
+	repo interfaces.Repository
+	log  interfaces.Logger
+}
+
+func NewUserUseCase(repo interfaces.Repository, log interfaces.Logger) UserUseCase {
+	return UserUseCase{
+		repo: repo,
+		log:  log,
+	}
+}
+
+func (uc UserUseCase) Create(ap domain.Payload) error {
+	return uc.repo.Insert(ap)
+}
+
+func (uc UserUseCase) Update(ap domain.Payload) error {
+	return uc.repo.Update(ap)
+}
+
+func (uc UserUseCase) Delete(ap domain.Payload) (domain.Payload, error) {
+	return uc.repo.Delete(ap)
+}
+
+func (uc UserUseCase) Read(ap domain.Payload) (domain.Payload, error) {
+	return uc.repo.Select(ap)
+}
